ca7cbdc469f336ee4e748efde757e05d41a713f8
Elliot <elliot.lenoa@laposte.net>
2022-09-15 11:25:36 +0200

    Menu si 0 ennemi

---
+++ Assets/Script/GameManagerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField] Canvas _canva;
+    public List<GameObject> _enemies = new List<GameObject>();
+    
+
+    private void Start()
+    {
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy").ToList() ;
+    }
+    
+
+    private void Update()
+    {
+        
+        StartCoroutine(CountEnemy());
+
+    }
+
+    IEnumerator CountEnemy()
+    {
+        yield return new WaitForSeconds(1);
+        _enemies.RemoveAll(go => go == null || !go.activeInHierarchy);
+        if (_enemies.Count == 0)
+        {
+            _canva.gameObject.SetActive(true);
+        }
+        
+    }
+}
